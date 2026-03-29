aef1fe8f4fe6098a6a7ccd181af56f72924ca4a1
Nikhil Agarwal <n5agarwa@uwaterloo.ca>
2026-03-29 13:14:32 -0400

    Time: 1 ms (99.98%) | Memory: 48.5 MB (67.61%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -14,22 +14,23 @@
  * }
  */
 class Solution {
+    /* BFS version
     public List<Double> averageOfLevels(TreeNode root) {
         List<Double> average = new ArrayList<>();
         if (root == null)
             return average;
-
+    
         Queue<TreeNode> bfs = new LinkedList<>();
         bfs.offer(root);
-
+    
         while (!bfs.isEmpty()) {
             int size = bfs.size();
             Double sum = 0.0;
-
+    
             for (int i = 0; i < size; i++) {
                 TreeNode node = bfs.poll();
                 sum += node.val;
-
+    
                 if (node.left != null)
                     bfs.offer(node.left);
                 if (node.right != null)
@@ -38,5 +39,34 @@ class Solution {
             average.add(sum/size);
         }
         return average;
+    }
+    */
+    //DFS version
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> sum = new ArrayList<>();
+        List<Integer> count = new ArrayList<>();
+        List<Double> result = new ArrayList<>();
+
+        dfs(root, 0, sum, count);
+
+        for(int i = 0; i < sum.size(); i++){
+            result.add(sum.get(i)/count.get(i));
+        }
+        return result;
+    }
+
+    private void dfs(TreeNode node, int level, List<Double> sum, List<Integer> count) {
+        if(node == null) return;
+
+        if(level == sum.size()){
+            sum.add((double)node.val); //first time visit node
+            count.add(1);
+        }
+        else{
+            sum.set(level, (double)sum.get(level) + node.val);
+            count.set(level, count.get(level) + 1);
+        }
+        dfs(node.left, level + 1, sum, count);
+        dfs(node.right, level + 1, sum, count);
     }
 }
