f3a9277d8999f3ee57c9a2e41f63f47e0e11157a
Nikhil Agarwal <n5agarwa@uwaterloo.ca>
2026-05-16 16:37:06 -0400

    Time: 16 ms (98.79%) | Memory: 90.1 MB (98.34%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,68 +1,47 @@
 class Solution {
+    int idx = 0;
+
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> graph = new ArrayList<>();
+        int[] head = new int[n];
+        Arrays.fill(head, -1);
 
-        // Create one adjacency list for each city.
-        for (int city = 0; city < n; city++) {
-            graph.add(new ArrayList<>());
-        }
+        int m = connections.length;
+        int[] to = new int[2 * m];
+        int[] next = new int[2 * m];
+        int[] cost = new int[2 * m];
 
-        for (int[] connection : connections) {
-            int from = connection[0];
-            int to = connection[1];
-
-            // Original road goes from "from" to "to".
-            // If we travel this same direction while moving away from city 0,
-            // this road must be reversed, so mark it with cost 1.
-            graph.get(from).add(new int[] { to, 1 });
-
-            // Traveling from "to" back to "from" already points toward city 0,
-            // so this direction does not need a reversal and has cost 0.
-            graph.get(to).add(new int[] { from, 0 });
+        for (int[] e : connections) {
+            add(e[0], e[1], 1, head, to, next, cost);
+            add(e[1], e[0], 0, head, to, next, cost);
         }
 
         boolean[] visited = new boolean[n];
-        Deque<Integer> stack = new ArrayDeque<>();
-        int reorderCount = 0;
+        return dfs(0, head, to, next, cost, visited);
+    }
 
-        // Start from city 0 and visit every city through the undirected tree.
-        visited[0] = true;
-        stack.push(0);
+    private void add(int from, int dest, int c,
+                     int[] head, int[] to, int[] next, int[] cost) {
+        to[idx] = dest;
+        cost[idx] = c;
+        next[idx] = head[from];
+        head[from] = idx++;
+    }
 
-        while (!stack.isEmpty()) {
-            int city = stack.pop();
+    private int dfs(int city, int[] head, int[] to, int[] next,
+                    int[] cost, boolean[] visited) {
+        visited[city] = true;
 
-            for (int[] edge : graph.get(city)) {
-                int nextCity = edge[0];
-                int reorderCost = edge[1];
+        int ans = 0;
 
-                if (visited[nextCity]) {
-                    continue;
-                }
+        for (int edge = head[city]; edge != -1; edge = next[edge]) {
+            int nextCity = to[edge];
 
-                // Add 1 only when the original road points away from city 0.
-                reorderCount += reorderCost;
-                visited[nextCity] = true;
-                stack.push(nextCity);
+            if (!visited[nextCity]) {
+                ans += cost[edge];
+                ans += dfs(nextCity, head, to, next, cost, visited);
             }
         }
 
-        return reorderCount;
-    }
-
-    private int dfs(int city, List<List<int[]>> graph, boolean visited[]) {
-        visited[city] = true;
-        int cost = 0;
-
-        for (int[] neighbour : graph.get(city)) {
-            int nextCity = neighbour[0];
-            int needReverse = neighbour[1];
-
-            if (visited[nextCity])
-                continue;
-            cost += needReverse;
-            cost += dfs(nextCity, graph, visited);
-        }
-        return cost;
+        return ans;
     }
 }
